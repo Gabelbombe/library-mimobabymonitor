d99f025418f9aa10dd84d1a97fe926063b8ab096
ehime <dodomeki@gmail.com>
2016-06-15 12:13:12 -0700

    Bad status codes

--- shell/requirements.sh
+++ shell/requirements.sh
@@ -5,25 +5,26 @@
 
 ## Declare versions comparing
 vercmp() {
-    local a="$1" b="$2"
-    local -i fa fb
+  local a="$1" b="$2"
+  local -i fa fb
 
-    while true; do
+  while true; do
 
-  fa="${a%%.*}"
-  fb="${b%%.*}"
+    fa="${a%%.*}"
+    fb="${b%%.*}"
 
-  ((fa != fb)) && break
+    ((fa != fb)) && break
 
-  a="${a#$fa}"; a="${a#.}"
-  b="${b#$fb}"; b="${b#.}"
+    a="${a#$fa}"; a="${a#.}"
+    b="${b#$fb}"; b="${b#.}"
 
-  if [[ -z $a && -z $b ]]; then
-      echo 0
-      return
-  fi
-    done
-    ((fa > fb)) && echo 1 || echo -1
+    if [[ -z $a && -z $b ]]; then
+      echo 0 ; return 0
+    fi
+
+  done
+
+  ((fa > fb)) && echo 1 || echo -1
 }
 
 
@@ -32,13 +33,14 @@ if ! $(which python |grep 'python is' |head -n1) ; then
   brew install python ## python ~2.7, python3 is the ~3.x package
 fi
 
+
 ## Test correct version, do not force upgrade incase they are at ~3.x
 if [[ $(vercmp $(python --version 2>&1 |awk '{print$2}') 2.7) -lt 0 ]]; then
-  echo "Python version is under 2.7, please upgrade it..." ; return 0
+  echo "Python version is under 2.7, please upgrade it..." ; return 1
 fi
 
 
 ## Test and install PIP if not available
 if ! $(which pip |grep 'pip is' |head -n1) ; then
-  brew install python ## python ~2.7, python3 is the ~3.x package
+  echo "Something broke with PIP install, exiting..." ; return 1
 fi
