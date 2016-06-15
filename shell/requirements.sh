c009cdd8e73bb9a4a15c0e6629601cbb2658954b
ehime <dodomeki@gmail.com>
2016-06-15 12:09:37 -0700

    Adding installer script up to pip test

---
+++ shell/requirements.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Assumes OSX at or greater than Yosemite
+# Assumes you are not rootless root
+# Assumes brew is installed
+
+## Declare versions comparing
+vercmp() {
+    local a="$1" b="$2"
+    local -i fa fb
+
+    while true; do
+
+  fa="${a%%.*}"
+  fb="${b%%.*}"
+
+  ((fa != fb)) && break
+
+  a="${a#$fa}"; a="${a#.}"
+  b="${b#$fb}"; b="${b#.}"
+
+  if [[ -z $a && -z $b ]]; then
+      echo 0
+      return
+  fi
+    done
+    ((fa > fb)) && echo 1 || echo -1
+}
+
+
+## Test and install Python if not available
+if ! $(which python |grep 'python is' |head -n1) ; then
+  brew install python ## python ~2.7, python3 is the ~3.x package
+fi
+
+## Test correct version, do not force upgrade incase they are at ~3.x
+if [[ $(vercmp $(python --version 2>&1 |awk '{print$2}') 2.7) -lt 0 ]]; then
+  echo "Python version is under 2.7, please upgrade it..." ; return 0
+fi
+
+
+## Test and install PIP if not available
+if ! $(which pip |grep 'pip is' |head -n1) ; then
+  brew install python ## python ~2.7, python3 is the ~3.x package
+fi
